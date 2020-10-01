98299e142023fa216410ef7d1e49e031cce69020
VivekJung <vivekjunghamal@gmail.com>
2020-10-01 12:41:46 +0545

    test : changed color

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Travel Budget',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.amber,
       ),
       home: LandingPage(),
     );
